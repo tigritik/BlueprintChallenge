074889af93d8084c6dffdf06d4703c474479bd7d
tigritik <tigritik@gmail.com>
2025-10-05 18:23:53 -0400

    created encryption page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,14 +3,22 @@ import './App.css'
 import TextBox from "./components/TextBox.tsx";
 
 function App() {
-  const [text, setText] = useState("");
+    const [key, setKey] = useState("");
+    const [payload, setPayload] = useState("");
 
-  return (
-    <>
-        <TextBox changeState={setText} />
-        <p>{text}</p>
-    </>
-  );
+    return (
+        <>
+            <h1>Encrypt Payload</h1>
+            <div>
+                <TextBox changeState={setKey} label="Public Key" id="key" />
+                <TextBox changeState={setPayload} label="Payload" id="payload" />
+            </div>
+            <p>{key}</p>
+            <button onClick={() => {
+                console.log("Payload", payload, "Key", key);
+            }}>Encrypt Payload</button>
+        </>
+    );
 }
 
 export default App;
--- web/src/components/TextBox.tsx
+++ web/src/components/TextBox.tsx
@@ -1,17 +1,24 @@
 import type {ChangeEvent} from "react";
 
 type TextBoxProps = {
-    changeState: (state: string) => void
+    changeState: (state: string) => void,
+    label?: string,
+    id?: string
 }
 
 function TextBox(props: TextBoxProps) {
 
     function onChangeText(e: ChangeEvent<HTMLInputElement>) {
-        props.changeState(e.currentTarget.value)
+        props.changeState(e.currentTarget.value);
     }
 
 
-    return <input onChange={onChangeText} />;
+    return (
+        <>
+            <label htmlFor={props.id}>{props.label}</label>
+            <input id={props.id} onChange={onChangeText} />
+        </>
+    );
 }
 
 export default TextBox;
